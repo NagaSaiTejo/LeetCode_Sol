591cde77a931a937558e66ae20ccf147d15504ea
Tejomurthula Naga Sai Satyanarayana Murthy <95856949+NagaSaiTejo@users.noreply.github.com>
2025-12-24 14:48:58 +0530

    Time: 49 ms (84.45%) | Memory: 21.2 MB (64.23%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -2,7 +2,7 @@ class Solution(object):
     def maxSubArray(self, nums):
         #ans = -math.inf
         # Or simply
-        ans = -99999
+        ans = float('-inf')
         sum=0
         for i in range(len(nums)):
             sum+=nums[i]
