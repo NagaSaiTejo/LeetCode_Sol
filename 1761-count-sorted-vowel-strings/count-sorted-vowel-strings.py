171774e26790623926ce7bc2efaec9116e2108ac
Tejomurthula Naga Sai Satyanarayana Murthy <95856949+NagaSaiTejo@users.noreply.github.com>
2026-05-29 11:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (20.00%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.py
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.py
@@ -1,11 +1,3 @@
 class Solution(object):
     def countVowelStrings(self, n):
-        a=e=i=o=u=1
-        n-=1
-        while n:
-            o+=u
-            i+=o
-            e+=i
-            a+=e
-            n-=1
-        return a+e+i+o+u
+        return ((n+1)*(n+2)*(n+3)*(n+4))//24
