1c1b13e6a576070bbee7e2d08e3f76a411ce285a
Tejomurthula Naga Sai Satyanarayana Murthy <95856949+NagaSaiTejo@users.noreply.github.com>
2026-06-04 11:54:30 +0530

    Time: 1225 ms (35.71%) | Memory: 15.1 MB (67.86%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -6,8 +6,10 @@ class Solution(object):
             si=len(s)
             c=0
             for j in range(1,si-1):
-                
-                if (int(s[j])>int(s[j-1]) and int(s[j])>int(s[j+1])) or (int(s[j])<int(s[j-1]) and int(s[j])<int(s[j+1])):
+                cur=int(s[j])
+                prevcur=int(s[j-1])
+                postcur=int(s[j+1])
+                if (cur>prevcur and cur>postcur) or (cur<prevcur and cur<postcur):
                     c+=1
             ans+=c
         return ans
