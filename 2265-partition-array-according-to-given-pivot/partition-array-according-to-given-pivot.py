49090a5ef6dce8084693e71a58ff8deacc93fe2f
Tejomurthula Naga Sai Satyanarayana Murthy <95856949+NagaSaiTejo@users.noreply.github.com>
2026-06-08 11:00:05 +0530

    Time: 48 ms (95.18%) | Memory: 28.3 MB (87.72%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,20 +1,32 @@
 class Solution(object):
     def pivotArray(self, nums, pivot):
-        arr=[0]*len(nums)
-        i=0
-        left=0
-        j=len(nums)-1
-        right=len(nums)-1
-        while i<len(nums) and j>=0:
-            if nums[i]<pivot:
-                arr[left]=nums[i]
-                left+=1
-            if nums[j]>pivot:
-                arr[right]=nums[j]
-                right-=1
-            i+=1
-            j-=1
-        while left<=right:
-            arr[left]=pivot
-            left+=1
-        return arr
+        l=[]
+        r=[]
+        c=0
+        for i in nums:
+            if i<pivot:
+                l.append(i)
+            elif i>pivot:
+                r.append(i)
+            else:
+                c+=1
+        r=l+ [pivot]*c +r
+        return r
+        # arr=[0]*len(nums)
+        # i=0
+        # left=0
+        # j=len(nums)-1
+        # right=len(nums)-1
+        # while i<len(nums) and j>=0:
+        #     if nums[i]<pivot:
+        #         arr[left]=nums[i]
+        #         left+=1
+        #     if nums[j]>pivot:
+        #         arr[right]=nums[j]
+        #         right-=1
+        #     i+=1
+        #     j-=1
+        # while left<=right:
+        #     arr[left]=pivot
+        #     left+=1
+        # return arr
