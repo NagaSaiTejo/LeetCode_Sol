e1ce1d6d7b552db10905e660347cbc4039780c02
Tejomurthula Naga Sai Satyanarayana Murthy <95856949+NagaSaiTejo@users.noreply.github.com>
2026-08-02 23:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (55.87%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,3 +1,5 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        evensum=sum(piles[i] for i in range(0,len(piles),2))
+        oddsum=sum(piles[i] for i in range(1,len(piles),2))
+        return evensum!=oddsum
