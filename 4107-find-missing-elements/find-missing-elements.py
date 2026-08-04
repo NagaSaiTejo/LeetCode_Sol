9b62112a9f9279e0a84fa97aeb518f203e253728
Tejomurthula Naga Sai Satyanarayana Murthy <95856949+NagaSaiTejo@users.noreply.github.com>
2026-08-04 14:52:12 +0530

    Time: 10 ms (5.96%) | Memory: 19.1 MB (97.28%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,10 +1,14 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        s=set(nums)
         mn=min(nums)
         mx=max(nums)
         res=[]
         for i in range(mn,mx+1):
-            if i not in s:
+            found=False
+            for x in nums:
+                if x==i:
+                    found=True
+                    break
+            if not found:
                 res.append(i)
         return res
